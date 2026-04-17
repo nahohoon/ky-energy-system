14526faf61dbc99cd26d19232f5eb0f743b89646
nahohoon <skghgns@gmail.com>
2026-04-17 15:14:26 +0900

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = 'ky-energy-secret-2026'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'ky_energy.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
